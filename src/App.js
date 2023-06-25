40f3e97784a5d641edfa21dde4781c3ae67f9fe0
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2023-06-25 09:20:27 +0530

    user dashboard frontend integrated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Home from './components/Home/Home';
+import Dashboard from './components/UserDashboard/Dashboard';
+import Login from './components/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GoToTop from './components/GoToTop/GoToTop';
 
@@ -11,6 +13,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path="/signin" element={<Login/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
           </Routes>
         </BrowserRouter>
  
